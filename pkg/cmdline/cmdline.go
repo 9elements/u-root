129834d12bbe80c71a4af2cbbf5b2f6b14ab0b2d
Christian Walter <christian.walter@9elements.com>
2025-03-02 21:11:37 +0100

    fix: clean up libinit code
    Signed-off-by: Christian Walter <christian.walter@9elements.com>

--- pkg/cmdline/cmdline.go
+++ pkg/cmdline/cmdline.go
@@ -222,10 +222,10 @@ func FlagsForModule(name string) string {
 
 // Consoles returns the list of all `console=` values in the kernel command line.
 func (c *CmdLine) Consoles() []string {
-	consoles := make([]string, 0)
-	for part := range strings.FieldsSeq(c.Raw) {
-		if after, ok := strings.CutPrefix(part, "console="); ok {
-			consoles = append(consoles, strings.Split(after, ",")[0])
+	var consoles []string
+	for _, part := range strings.Fields(c.Raw) {
+		if strings.HasPrefix(part, "console=") {
+			consoles = append(consoles, strings.Split(strings.TrimPrefix(part, "console="), ",")[0])
 		}
 	}
 	return consoles
